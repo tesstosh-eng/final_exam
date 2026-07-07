eb097c4b2159fe54e4673f29b0a61c57b3f03a81
Anastasiia <smail55@yandex.com>
2026-07-07 22:11:58 +0300

    Дополнено

--- configuration.py
+++ configuration.py
@@ -1,11 +1,9 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
 
-URL_SERVICE = "https://2d82b1f7-bcea-4ce9-a7c9-8723fb43586f.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://6933b01a-2afc-4de7-ae83-be28751fb53f.serverhub.praktikum-services.ru"
 
-# CREATE_ORDERS хранит путь к API-методу для создания нового заказа.
-# Этот путь будет использоваться для формирования полного URL-адреса в сочетании с базовым URL сервиса, когда необходимо выполнить запрос на создание заказа.
+# CREATE_ORDER хранит путь к API-методу для создания нового заказа.
 CREATE_ORDERS = "/api/v1/orders"
 
 # GET_ORDER_BY_TRACK хранит путь к API-методу для получения нового заказа.
-# Этот путь будет использоваться для формирования полного URL-адреса в сочетании с базовым URL сервиса, когда необходимо выполнить запрос на получение заказа.
-GET_ORDER_BY_TRACK = "/orders/track"
+GET_ORDER_BY_TRACK = "/api/v1/orders/track"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,24 +1,51 @@
-# Импорт модуля requests для отправки HTTP-запросов
-import requests
-# Импорт конфигурационного файла, который содержит настройки URL
+# Импорт настроек из модуля configuration, который содержит параметры конфигурации, такие как URL сервиса
 import configuration
+
+# Импорт библиотеки requests для выполнения HTTP-запросов
+import requests
+
 # Импорт данных запроса из модуля data, в котором определены заголовки и тело запроса
-from data import order_body  
+import data 
 
 # Запрос на создание заказа.
 # Определение функции create_order для отправки POST-запроса на создание нового заказа
-def create_order(order_data):
-    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
-    # URL_SERVICE и CREAT_ORDERS объединяются для формирования полного URL для запроса
-    # json=body используется для отправки данных пользователя в формате JSON
-    response = requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDERS,
-                         json=order_data)
-    return response
-
+def post_create_order(body):
+    # Выполнение POST-запроса на создание заказа
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDERS,
+                         json=body,
+                         headers=data.headers)
+    
 # Выполняет запрос на получение заказа по трек-номеру.
 # Определение функции для отправки GET-запроса получение номера заказа. С параметром track_number.
-def get_order_by_track(track_number):
-    get_order_url = f"{configuration.URL_SERVICE}{configuration.GET_ORDER_BY_TRACK}"
-    # Вызов функции requests.get с телом запроса для создания нового заказа из модуля data
-    response = requests.get(get_order_url, params={"t": track_number})
-    return response
+def get_order_by_track(track):
+    # Выполнение GET-запроса на получение заказа
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK, 
+                        headers=data.headers, 
+                        params={"t": track})
+
+# Функция для позитивной проверки
+def positive_assert_order():
+    # В переменную order_body сохраняется тело запроса на создание заказа
+    order_body = data.order_body
+    # В переменную create_response сохраняется результат запроса на создание заказа
+    create_response = post_create_order(order_body)
+
+    # Проверяется, что код ответа равен 200 или 201
+    assert create_response.status_code in (200, 201), "Не удалось создать заказ"
+
+    # Сохранение трек-номера
+    track_number = create_response.json().get("track")
+    print("Заказ создан, Номер трека:", track_number)
+
+    # Проверяется, что трек-номер получен и не является пустым
+    assert track_number, "Трек-номер не получен"
+
+    # Получение заказа по трек-номеру
+    get_response = get_order_by_track(track_number)
+
+    # Проверка статуса ответа
+    assert get_response.status_code == 200, "Заказ не найден"
+    print("Тест успешно пройден. Код 200.")
+
+if __name__ == "__main__":
+    positive_assert_order()
